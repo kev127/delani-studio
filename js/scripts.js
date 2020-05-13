10ed9f4bb1163c63c84a170fc979e6362f003886
kev127 <kelvinkeya125@gmail.com>
2020-05-13 14:08:41 +0300

    made changes in the files

--- js/scripts.js
+++ js/scripts.js
@@ -1,104 +1,82 @@
 $(document).ready(function() {
-    $("designlogo").click(function() {
- $(".logo").toggle();
- $(".designlogo").toggle();
+    $("#designlogo").click(function() {
+    $("#toogleDesignlogo").toggle();
+  });
+  $("#toogleDesignlogo").click(function(){
+    $("#toogleDesignlogo").toggle();
+    $("#designlogo").toggle();
+     });
+  });
+
+  $(document).ready(function() {
+  $("#developmentlogo").click(function() {
+    $("#toogleDevelopmentlogo").toggle();
     });
- });
- $(document).ready(function() {
-     $("#par1").click(function() {
-  $(".logo").toggle();
-  $(".designlogo").toggle();
+  $("#toogleDevelopmentlogo").click(function() {
+    $("#toogleDevelopmentlogo").toggle();
+    $("#developmentlogo").toggle();
      });
   });
- 
- $(document).ready(function() {
-     $("ProductManagementlogo").click(function() {
-  $(".logo2").toggle();
-  $(".ProductManagementlogo").toggle();
-     });
-  });
+
   $(document).ready(function() {
-     $("part3").click(function() {
-  $(".logo2").toggle()
-  ;$(".ProductManagementlogo").toggle();
-     });
+    $("#ProductManagementlogo").click(function() {
+    $("#toogleProductManagementlogo").toggle();
   });
-  $(document).ready(function() {
-     $("developmentlogo").click(function() {
-  $(".logo1").toggle();
-  $(".part2").toggle();
+  $("#toogleProductManagementlogo").click(function(){
+    $("#toogleProductManagementlogo").toggle();
+    $("#ProductManagementlogo").toggle();
      });
-  });
-  $(document).ready(function() {
-     $("part3").click(function() {
-  $(".part3").toggle();
-  $(".developmentlogo").toggle();
-     });
-  });
-$(document).ready(function(){
-    $("form").submit(function(){
-        var name = document.getElementById("name");
-        var email = document.getElementById("email");
-        var text = document.getElementById("textarea");
-        if (name.value == "" || email.value == "" || text.value == "") {
-            alert("undefined");
-        }
-        else {
-      alert("Hey " + name.value + "! We have received your message,Thank You!");
-    }
-    event.preventDefault();
-    });
   });
 
   $(document).ready(function(){
    $("#work1").mouseover(function(){
-     $("#work1").show();
+     $("#work1").fadeIn();
    }).mouseout(function(){
-     $("#work1").hide();
+     $("#work1").fadeOut();
    });
  });
  $(document).ready(function(){
    $("#work2").mouseover(function(){
-     $("#work2").show();
+     $("#work2").fadeIn();
    }).mouseout(function(){
-     $("#work2").hide();
+     $("#work2").fadeOut();
    });
  });
  $(document).ready(function(){
    $("#work3").mouseover(function(){
-     $("#work3").show();
+     $("#work3").fadeIn();
    }).mouseout(function(){
-     $("#work3").hide();
+     $("#work3").fadeOut();
    });
  });
  $(document).ready(function(){
    $("#work4").mouseover(function(){
-     $("#work4").show();
+     $("#work4").fadeIn();
    }).mouseout(function(){
-     $("#owork4").hide();
+     $("#work4").fadeOut();
    });
  });
  
  $(document).ready(function(){
    $("#work5").mouseover(function(){
-     $("#work5").show();
+     $("#work5").fadeIn();
    }).mouseout(function(){
-     $("#work5").hide();
+     $("#work5").fadeOut();
    });
    $("#work6").mouseover(function(){
-     $("#work6").show();
+     $("#work6").fadeIn();
    }).mouseout(function(){
-     $("#work6").hide();
+     $("#work6").fadeOut();
    });
    $("#work7").mouseover(function(){
-     $("#work7").show();
+     $("#work7").fadeIn();
    }).mouseout(function(){
-     $("#work7").hide();
+     $("#work7").fadeOut();
    });
    $("#work8").mouseover(function(){
-     $("#work8").show();
+     $("#work8").fadeIn();
    }).mouseout(function(){
-     $("#work8").hide();
+     $("#work8").fadeOut();
    });
  });
  
@@ -108,11 +86,12 @@ $(document).ready(function(){
      var name = $("input").val();
      var email = $("input").val();
      var message = $("textarea").val();
-    
+     if (inputName=="" || inputEmail=="" || x==""){
      alert (name + ", we have received your message. Thank you for reaching out to us.");
-     
+    }
      else {
-       alert("Please enter your name and email!");
+     
+      alert('Please enter your name and email!');
      }
      
    });
